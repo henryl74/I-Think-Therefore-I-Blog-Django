300ace01ecab3bb0b74f354b13d16d39e9152d7c
Henry Leon <henryatusabes@gmail.com>
2022-10-31 21:27:55 +0000

    Add HTML templates, and style.css files, updated views.py to be able to view POST

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+# from django.http import HttpResponseRedirect
+# from django.contrib import messages
+from .models import Post
+# from .forms import CommentForm
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
